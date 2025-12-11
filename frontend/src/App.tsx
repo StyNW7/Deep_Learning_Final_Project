6e901c5c3f8ce1adf297c322f1d066ce3033a2fe
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-12-11 09:36:15 +0700

    feat: add logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router';
-// import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 import MapPage from './pages/MapPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<MapPage />} />
       </Routes>
     </BrowserRouter>
